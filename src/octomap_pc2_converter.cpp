5c64602efd994f36209ce8503c3f6b9b89f7c6a9
Kyle Harlow <kyle.harlow@colorado.edu>
2019-08-11 17:13:06 -0600

    Fixed compilation/pointer issue

--- src/octomap_pc2_converter.cpp
+++ src/octomap_pc2_converter.cpp
@@ -35,8 +35,8 @@ void conversionCallback(const octomap_msgs::OctomapConstPtr& octomap_in){
 		}
 		pcl::toROSMsg(*cloud_in,*pc2_out);
 		
-		*pc2_out.header.stamp = ros::Time::now();
-		*pc2_out.header.frame_id = "world";
+		pc2_out->header.stamp = ros::Time::now();
+		pc2_out->header.frame_id = "world";
 		pub.publish(*pc2_out);
 	} else {
 		ROS_ERROR("Error reading OcTree from abstract");
